fa8a7b0d8d233df7f4e9eaee60c74e8fb3615f1f
Edward Beech <initialed85@gmail.com>
2020-02-05 13:32:05 +0800

    More tweaking

--- carla_multiplayer/sensor.py
+++ carla_multiplayer/sensor.py
@@ -21,7 +21,7 @@ _HEIGHT = 360
 _SENSOR_BLUEPRINT_NAME = 'sensor.camera.rgb'
 _TRANSFORM = carla.Transform(
     carla.Location(-15, 0, 7.5),
-    carla.Rotation(0, 0, -10)
+    carla.Rotation(-10, 0, 0)
 )
 
 
