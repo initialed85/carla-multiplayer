9e48fca6001a72b47a1f72527fe3a79c120805cd
Edward Beech <initialed85@gmail.com>
2020-02-07 09:16:17 +0800

    Missing file

---
+++ carla_multiplayer/wrapped_carla.py
@@ -0,0 +1,82 @@
+import os
+import sys
+
+sys.path.append(
+    '{}/carla-0.9.6-py3.6-linux-x86_64.egg'.format(
+        os.path.dirname(os.path.realpath(__file__))
+    )
+)
+
+try:
+    from carla import *
+except ImportError as e:
+    print(1, e)
+    try:
+        from .carla import *
+    except ImportError as e:  # hack for platforms that don't support or don't have Carla
+        print(2, e)
+        print('warning: failed to import carla because {}; objects are mocked'.format(repr(e)))
+
+    from mock import MagicMock
+
+    Actor = MagicMock()
+    ActorAttribute = MagicMock()
+    ActorAttributeType = MagicMock()
+    ActorBlueprint = MagicMock()
+    ActorList = MagicMock()
+    ActorSnapshot = MagicMock()
+    AttachmentType = MagicMock()
+    BlueprintLibrary = MagicMock()
+    BoundingBox = MagicMock()
+    Client = MagicMock()
+    ClientSideSensor = MagicMock()
+    CollisionEvent = MagicMock()
+    Color = MagicMock()
+    ColorConverter = MagicMock()
+    DebugHelper = MagicMock()
+    GearPhysicsControl = MagicMock()
+    GeoLocation = MagicMock()
+    GnssEvent = MagicMock()
+    GnssSensor = MagicMock()
+    Image = MagicMock()
+    LaneChange = MagicMock()
+    LaneInvasionEvent = MagicMock()
+    LaneInvasionSensor = MagicMock()
+    LaneMarking = MagicMock()
+    LaneMarkingColor = MagicMock()
+    LaneMarkingType = MagicMock()
+    LaneType = MagicMock()
+    LidarMeasurement = MagicMock()
+    Location = MagicMock()
+    Map = MagicMock()
+    ObstacleDetectionEvent = MagicMock()
+    Rotation = MagicMock()
+    Sensor = MagicMock()
+    SensorData = MagicMock()
+    ServerSideSensor = MagicMock()
+    Timestamp = MagicMock()
+    TrafficLight = MagicMock()
+    TrafficLightState = MagicMock()
+    TrafficSign = MagicMock()
+    Transform = MagicMock()
+    Vector2D = MagicMock()
+    Vector3D = MagicMock()
+    Vehicle = MagicMock()
+    VehicleControl = MagicMock()
+    VehiclePhysicsControl = MagicMock()
+    Walker = MagicMock()
+    WalkerAIController = MagicMock()
+    WalkerBoneControl = MagicMock()
+    WalkerControl = MagicMock()
+    Waypoint = MagicMock()
+    WeatherParameters = MagicMock()
+    WheelPhysicsControl = MagicMock()
+    World = MagicMock()
+    WorldSettings = MagicMock()
+    WorldSnapshot = MagicMock()
+    command = MagicMock()
+    libcarla = MagicMock()
+    vector_of_gears = MagicMock()
+    vector_of_ints = MagicMock()
+    vector_of_vector2D = MagicMock()
+    vector_of_wheels = MagicMock()
